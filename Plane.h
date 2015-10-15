f391d129b53b1d2417a8b627b4148ecca6b3c298
Jakob Törmä Ruhl <jakob.ruhl@gmail.com>
2015-10-15 13:36:17 +0200

    edited the plane equation to also give area and that you can move in the plane with an offset

--- Plane.h
+++ Plane.h
@@ -20,20 +20,43 @@ public:
 			points.push_back(glm::vec3(position.x - size_x, position.y, position.z + size_z));
 			points.push_back(glm::vec3(position.x + size_x, position.y, position.z - size_z));
 			points.push_back(glm::vec3(position.x - size_x, position.y, position.z - size_z));
+			_area = size_x*size_z;
 		}
-		if (normal == glm::vec3(1,0,0) || normal == glm::vec3(-1, 0, 0))
+		else if (normal == glm::vec3(1,0,0) || normal == glm::vec3(-1, 0, 0))
 		{
 			points.push_back(glm::vec3(position.x, position.y + size_y, position.z + size_z));
 			points.push_back(glm::vec3(position.x, position.y - size_y, position.z + size_z));
 			points.push_back(glm::vec3(position.x, position.y + size_y, position.z - size_z));
 			points.push_back(glm::vec3(position.x, position.y - size_y, position.z - size_z));
+			_area = size_y*size_z;
 		}
-		if (normal == glm::vec3(0, 0, 1) || normal == glm::vec3(0, 0, -1))
+		else if (normal == glm::vec3(0, 0, 1) || normal == glm::vec3(0, 0, -1))
 		{
 			points.push_back(glm::vec3(position.x + size_x, position.y + size_y, position.z));
 			points.push_back(glm::vec3(position.x - size_x, position.y + size_y, position.z));
 			points.push_back(glm::vec3(position.x + size_x, position.y - size_y, position.z));
 			points.push_back(glm::vec3(position.x - size_x, position.y - size_y, position.z));
+			_area = size_x*size_y;
+		}
+		else
+		{
+			_area = 0;
+		}
+	};
+	
+	glm::vec3 GetOffsetOnPlane(float x, float y)
+	{
+		if (_normal == glm::vec3(0,1,0) || _normal == glm::vec3(0,-1,0))
+		{
+			return glm::vec3(_position.x + x, _position.y, _position.z + y);
+		}
+		else if (_normal == glm::vec3(1,0,0) || _normal == glm::vec3(-1, 0, 0))
+		{
+			return glm::vec3(_position.x, _position.y + x, _position.z + y);
+		}
+		else if (_normal == glm::vec3(0, 0, 1) || _normal == glm::vec3(0, 0, -1))
+		{
+			return glm::vec3(_position.x + x, _position.y + y, _position.z);
 		}
 	};
 
@@ -43,8 +66,8 @@ public:
 
 
 	std::vector<glm::vec3> points;
+	float _area;
 private:
-	
 	glm::vec3 _normal;
 	float _size_x;
 	float _size_y;
